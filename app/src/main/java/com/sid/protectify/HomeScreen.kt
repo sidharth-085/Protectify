ff9eaf66ef2eac8fbc9216a5e3e5c7f38c5d7448
Sidharth Kapoor <sidharthkapoor085@gmail.com>
2024-05-25 15:10:39 +0530

    Made Guard Fragment

--- app/src/main/java/com/sid/protectify/HomeScreen.kt
+++ app/src/main/java/com/sid/protectify/HomeScreen.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationBar.setOnItemSelectedListener { menuItem ->
+            if (menuItem.itemId == R.id.nav_guard) {
+                inflateFragment()
+            }
+
+            true
         }
+    }
+
+    private fun inflateFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, GuardFragment.newInstance())
+        transaction.commit()
     }
 }
